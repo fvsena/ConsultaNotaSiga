cb19e35acc689400ebb6477bab7cce29c1331e8e
Felipe Sena <dev_fvsena@outlook.com>
2019-09-17 09:04:14 -0300

    Produção 1.0.0.0

---
+++ RoboSiga/Helpers/MailHelper.cs
@@ -0,0 +1,34 @@
+﻿using System.Net.Mail;
+
+namespace RoboSiga.Helpers
+{
+    class MailHelper
+    {
+        public string SenderMail { get; set; }
+        public string Smtp { get; set; }
+        public string Password { get; set; }
+
+        public MailHelper(string senderMail, string password, string smtpServer)
+        {
+            this.SenderMail = senderMail;
+            this.Password = password;
+            this.Smtp = smtpServer;
+        }
+
+        public void SendMail(string receiverMail, string subject, string body)
+        {
+            MailMessage mail = new MailMessage();
+            mail.From = new MailAddress(this.SenderMail);
+            mail.To.Add(receiverMail);
+            mail.Subject = subject;
+            mail.Body = body;
+            SmtpClient smtp = new SmtpClient(this.Smtp);
+            smtp.Port = 587;
+            smtp.UseDefaultCredentials = false;
+            smtp.Credentials = new System.Net.NetworkCredential(this.SenderMail, this.Password);
+            smtp.EnableSsl = true;
+
+            smtp.Send(mail);
+        }
+    }
+}
---
+++ RoboSiga/Process/ProcessManager.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Threading;
+
+namespace RoboSiga.Process
+{
+    public class ProcessManager
+    {
+        public void Start(int delay)
+        {
+            while (true)
+            {
+                ExecuteProcess();
+                Console.WriteLine($" | Próxima execução: {DateTime.Now.AddSeconds(delay).ToString("dd/MM/yyyy hh:mm:ss")}");
+                Thread.Sleep(delay * 1000);
+            }
+        }
+
+        private void ExecuteProcess()
+        {
+            Console.Write(new SigaProcess().Start().Message);
+        }
+    }
+}
